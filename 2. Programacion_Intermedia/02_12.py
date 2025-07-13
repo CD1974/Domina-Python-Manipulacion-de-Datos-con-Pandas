472b60080243a27b7b0482dc7e97ef32cb1de521
CD1974 <mjcd1973@gmail.com>
2025-07-13 10:48:48 +0200

    updates empleados.csv

--- 2. Programacion_Intermedia/02_12.py
+++ 2. Programacion_Intermedia/02_12.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 archivo = 'empleados.csv'
-df_empleados = pd.read_csv(archivo, delimiter=";")
+df_empleados = pd.read_csv(archivo, delimiter=",")
 
 print("Dataframe original:")
 print(df_empleados)
