f075107616386a529a10617ccc1f8a3b3842ecd8
CD1974 <mjcd1973@gmail.com>
2025-07-14 23:08:21 +0200

    update 02_13

--- 2. Programacion_Intermedia/02_13.py
+++ 2. Programacion_Intermedia/02_13.py
@@ -1,1 +1,11 @@
-# Archivo generado autom�ticamente
+import pandas as pd
+
+archivo = 'empleados.csv'
+df_empleados = pd.read_csv(archivo, delimiter=",")
+
+print("Dataframe original:")
+print(df_empleados)
+
+df_empleados = df_empleados[df_empleados['Experiencia'] >= 5]
+print("Dataframe con empleados de 5 o más años de experiencia:")
+print(df_empleados)
