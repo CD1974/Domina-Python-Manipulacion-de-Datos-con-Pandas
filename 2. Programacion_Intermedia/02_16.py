4b2446e466509354c9e9368e2866fea8aa57c034
CD1974 <mjcd1973@gmail.com>
2025-07-14 23:13:54 +0200

    updates 02_14/15/16

--- 2. Programacion_Intermedia/02_16.py
+++ 2. Programacion_Intermedia/02_16.py
@@ -1,1 +1,18 @@
-# Archivo generado autom�ticamente
+# Manejo de agrupamiento sobre dataframes en Pandas
+
+import pandas as pd
+
+df_ventas = pd.DataFrame({
+    'productos': ['computadora', 'teléfono', 'computadora', 'teléfono', 'computadora', 'teléfono', 'computadora', 'teléfono' ],
+    'mes': ['Enero', 'Enero', 'Febrero', 'Febrero', 'Marzo', 'Marzo', 'Abril', 'Abril'],
+    'ventas': [10000, 12000, 15000, 18000, 20000, 22000, 15000, 18000]
+})
+
+print("Dataframe original:")
+print(df_ventas)
+
+grupos = df_ventas.groupby('productos')
+resultados = grupos.agg({'ventas': ['mean', 'sum']})
+
+print("Resultado del agrupamiento")
+print(resultados)
